d04314614ab8acc604c455b3622f3ea2ac75fd4c
Gayatri <takawalegayatri@gmail.com>
2025-08-02 13:51:25 +0530

    Add hosted backend url

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://erms-mt07.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
